1e30b4f13ecb40f7cacec4f545ec83ff15bbf22d
Mayurkeswani93 <mayurkeswani2001@gmail.com>
2021-05-16 09:59:07 +0530

    copying pincode to clipboard and download  faqs

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.css'
 const Footer = () =>{
 	return(
-		<footer className="footer">
+		<footer className="footer p-0">
         <pre>
           सिमरउ सिमरि सिमरि सुखु पावउ ॥<br /> कलि कलेस तन माहि मिटावउ ॥
         </pre>
